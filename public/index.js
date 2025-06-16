7be705299f1dd9ab9cf0712dfb9ba08b24c63534
mrwanlop678 <mrwanlop@icloud.com>
2025-06-16 15:43:32 +0700

    Update serve path in index.js

--- public/index.js
+++ public/index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended: true }))
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Route to serve index.html for the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html')); // Look for index.html directly in __dirname
 });
 
 const PORT = process.env.PORT || 3000
