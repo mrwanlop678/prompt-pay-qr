b6fe9651abc98616e0a215c1518b8c651759acab
mrwanlop678 <mrwanlop@icloud.com>
2025-06-16 15:36:15 +0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -10,6 +10,14 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended: true }))
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve index.html for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
